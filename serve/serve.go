c6eb02c3c681ad0218df7fede15dcac40f699841
Nick Cao <nickcao@nichi.co>
2022-03-25 17:03:30 +0800

    serve: better mtime handling

--- serve/serve.go
+++ serve/serve.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 var addr = flag.String("l", "[::]:8080", "listen address")
@@ -11,10 +13,38 @@ var path = flag.String("p", "./", "path to serve")
 
 func main() {
 	flag.Parse()
-	fs := http.FileServer(http.Dir(*path))
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.Header.Del("If-Modified-Since")
-		fs.ServeHTTP(w, r)
+	fs := http.FileServer(&FileSystem{
+		FileSystem: http.Dir(*path),
+		mod:        time.Now(),
 	})
-	log.Fatal(http.ListenAndServe(*addr, handler))
+	log.Fatal(http.ListenAndServe(*addr, fs))
+}
+
+type FileSystem struct {
+	http.FileSystem
+	mod time.Time
+}
+
+func (f *FileSystem) Open(name string) (http.File, error) {
+	file, err := f.FileSystem.Open(name)
+	return &File{File: file, mod: f.mod}, err
+}
+
+type File struct {
+	http.File
+	mod time.Time
+}
+
+func (f *File) Stat() (os.FileInfo, error) {
+	fileInfo, err := f.File.Stat()
+	return &FileInfo{FileInfo: fileInfo, mod: f.mod}, err
+}
+
+type FileInfo struct {
+	os.FileInfo
+	mod time.Time
+}
+
+func (f *FileInfo) ModTime() time.Time {
+	return f.mod
 }
